ed41a56853f89c99cccc00265fff62e155d0754e
zhouhongwei <18252055872@163.com>
2020-10-28 16:55:09 +0800

    解决跨域问题
    统一Response响应处理
    
    Api-Resource资源返回
    
    使用Enum枚举
    
    jwt-auth用户认证与无感知自动刷新
    
    jwt-auth多角色认证不串号
    
    单一设备登陆

--- api/app/Http/Controllers/Api/AdminController.php
+++ api/app/Http/Controllers/Api/AdminController.php
@@ -15,7 +15,7 @@ class AdminController extends Controller
     //返回用户列表
     public function index()
     {
-        if (Auth::guard('admin')->guest()) {
+        if (Auth::guest()) {
             return $this->failed('未登录');
         } else {
             //3个用户为一页
@@ -35,11 +35,25 @@ class AdminController extends Controller
         Admin::create($request->all());
         return $this->setStatusCode(201)->success('用户注册成功...');
     }
+
     //用户登录
     public function login(Request $request)
     {
-        $token = Auth::guard('api')->attempt(['name' => $request->name, 'password' => $request->password]);
+        //获取当前守护的名称
+        $present_guard = Auth::getDefaultDriver();
+        $token = Auth::claims(['guard' => $present_guard])->attempt(['name' => $request->name, 'password' => $request->password]);
         if ($token) {
+            //如果登陆，先检查原先是否有存token，有的话先失效，然后再存入最新的token
+            $user = Auth::user();
+            if ($user->last_token) {
+                try {
+                    Auth::setToken($user->last_token)->invalidate();
+                } catch (TokenExpiredException $e) {
+                    //因为让一个过期的token再失效，会抛出异常，所以我们捕捉异常，不需要做任何处理
+                }
+            }
+            $user->last_token = $token;
+            $user->save();
             return $this->setStatusCode(201)->success(['token' => 'bearer ' . $token]);
         }
         return $this->failed('账号或密码错误', 400);
@@ -47,13 +61,13 @@ class AdminController extends Controller
     //用户退出
     public function logout()
     {
-        Auth::guard('api')->logout();
+        Auth::logout();
         return $this->success('退出成功...');
     }
     //返回当前登录用户信息
     public function info()
     {
-        $user = Auth::guard('api')->user();
-        return $this->success(new AdminResource($user));
+        $admins = Auth::user();
+        return $this->success(new AdminResource($admins));
     }
 }
