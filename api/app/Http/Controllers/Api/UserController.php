af95811793176f74952c08741a795655c11d9ad7
zhouhongwei <18252055872@163.com>
2020-10-28 10:37:53 +0800

    初始化

---
+++ api/app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Requests\Api\UserRequest;
+use App\Http\Resources\Api\UserResource;
+use App\Models\Enum\UserEnum;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+
+    //返回用户列表
+    public function index()
+    {
+        if (Auth::guard('api')->guest()) {
+            return $this->failed('未登录');
+        } else {
+            //3个用户为一页
+            $users = User::paginate(3);
+            // return $this->success($users);
+            return UserResource::collection($users);
+        }
+    }
+    //返回单一用户信息
+    public function show(User $user)
+    {
+        return $this->success(new UserResource($user));
+    }
+    //用户注册
+    public function store(UserRequest $request)
+    {
+        dd(222);die;
+        User::create($request->all());
+        return $this->setStatusCode(201)->success('用户注册成功...');
+    }
+    //用户登录
+    public function login(Request $request)
+    {
+        $token = Auth::guard('api')->attempt(['name' => $request->name, 'password' => $request->password]);
+        if ($token) {
+            return $this->setStatusCode(201)->success(['token' => 'bearer ' . $token]);
+        }
+        return $this->failed('账号或密码错误', 400);
+    }
+    //用户退出
+    public function logout()
+    {
+        Auth::guard('api')->logout();
+        return $this->success('退出成功...');
+    }
+    //返回当前登录用户信息
+    public function info()
+    {
+        $user = Auth::guard('api')->user();
+        return $this->success(new UserResource($user));
+    }
+}
